be5819d91524672245b27bc81b7805031225a9c0
Dean Blackborough <dean@g3d-development.com>
2017-05-30 20:32:53 +0100

    Code hinters
    * Code hinting classes

--- src/Zend.php
+++ src/Zend.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * Code helper hinting helper class for views, in the view add a type hint to $this and
  * you will get code hinting help for any view helpers you add methods for.
  *
- * @example $this DBlackborough\Zf3ViewHelpersCC\CustomAndZend
+ * @example $this DBlackborough\Zf3ViewHelpersCC\Zend
  *
  * @note The view helpers typically use invoke, in this class we add the phpDoc for the
  * invoke method as though it was the constructor.
@@ -16,8 +16,6 @@ declare(strict_types=1);
  */
 
 namespace DBlackborough\Zf3ViewHelpersCC;
-
-use DBlackborough\Zf3ViewHelpers;
 
 use Zend\View\Helper\Cycle;
 use Zend\View\Helper\Doctype;
@@ -39,43 +37,8 @@ use Zend\View\Helper\ViewModel;
  *
  * @package Application\View\Helper
  */
-class CustomAndZend
+class Zend
 {
-    /**
-     * Create a Bootstrap 4 badge
-     *
-     * @param string $label
-     *
-     * @return Zf3ViewHelpers\Bootstrap4Badge
-     */
-    public function bootstrap4Badge(string $label) : Zf3ViewHelpers\Bootstrap4Badge {}
-
-    /**
-     * Create a Bootstrap 4 button
-     *
-     * @param string $label
-     *
-     * @return Zf3ViewHelpers\Bootstrap4Button
-     */
-    public function bootstrap4Button(string $label) : Zf3ViewHelpers\Bootstrap4Button {}
-
-    /**
-     * Create a Bootstrap 4 jumbotron component
-     *
-     * @param string $heading
-     * @param string $content
-     *
-     * @return Zf3ViewHelpers\Bootstrap4Jumbotron
-     */
-    public function bootstrap4Jumbotron(string $heading, string $content) : Zf3ViewHelpers\Bootstrap4Jumbotron {}
-
-    /**
-     * Create a Bootstrap 4 navbar, lite version
-     *
-     * @return Zf3ViewHelpers\Bootstrap4NavbarLite
-     */
-    public function bootstrap4NavbarLite() : Zf3ViewHelpers\Bootstrap4NavbarLite {}
-
     /**
      * @param string $asset
      *
